b29c08cb00b5d32ddddc13b18baef79a25722c96
Artur <morozarturv@gmail.com>
2013-01-09 09:59:08 +0200

    2

--- js/common.js
+++ js/common.js
@@ -7,7 +7,8 @@ var wnd_height = $(window).height();
 function cover() {	
 	var wnd_width = $(window).width();
 	var wnd_height = $(window).height();
-	$('.cover, .cover li').css({'width': wnd_width + 'px', 'height': wnd_height - 121 + 'px'});
+	$('.cover ul, .cover li, .news').css({'width': wnd_width + 'px', 'height': wnd_height - 121 + 'px'});
+	$('.news__list').css({'width': wnd_width - 20 + 'px', 'height': wnd_height - 141 + 'px'});
 }
 //cover details
 function cover_detail() {
@@ -17,17 +18,20 @@ function cover_detail() {
 }
 //cover slider
 function cover_slider() {
-	$('.cover').carouFredSel({
-		items: 1,
-		prev: ".cover-nav__prev",
-		next: ".cover-nav__next",
-		scroll: {
-			easing: "easeInOutCirc"
-		},
-		auto: {
-			duration: 500,
-			timeoutDuration: 4000
-		}
+	$('.cover ul').cycle({ 
+	    fx:     'fade', 
+	    speed:   1000, 
+	    timeout: 3000
+	});
+}
+//news scroll
+function news_scroll() {
+	$('.news__list').jScrollPane();
+}
+//news masonry blocks
+function news_blocks() {
+	$('.news__list ul').masonry({
+		itemSelector: '.news__list li'
 	});
 }
 //functions
@@ -38,9 +42,27 @@ cover_slider();
 $(window).resize(function () {
 	cover();
 	cover_detail();
-	cover_slider();
+	cover_slider();	
+	news_blocks();
+	news_scroll();
 });
-//
+//news open
+$('.nav__new').click(function() {
+	if ($(this).hasClass('nav__new_is')) {
+		$(this).removeClass('nav__new_is');
+	};
+	$('.news').slideDown();
+	news_blocks();
+	news_scroll();
+	return false;
+});
+//news close
+$('.news__close').click(function() {
+	$('.news').slideUp();
+	return false;
+});
+
+
 
 
 });
